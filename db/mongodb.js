ab35ff6ae7f28bbd720556f29ea8bd5f120b0982
wanlexiang <wanlexiang@zmeng123.com>
2018-12-13 11:18:51 +0800

    fix

--- db/mongodb.js
+++ db/mongodb.js
@@ -7,32 +7,38 @@ import config from 'config-lite';
 // chalk.<style>[.<style>...](string,[string...])
 import chalk from 'chalk';
 console.log(
-  chalk.red(config.url)
+	chalk.red(config.url)
 );
-mongoose.connect(config.url, {useMongoClient:true});
+mongoose.connect(config.url, {
+	useMongoClient: true
+});
 // 使用原生promise，mongoose自带promise不再支持了
 mongoose.Promise = global.Promise;
 
 const db = mongoose.connection;
 
-db.once('open' ,() => {
+db.once('open', () => {
 	console.log(
-    chalk.greenBright('连接数据库成功')
-  );
+		chalk.greenBright('连接数据库成功')
+	);
 })
 
-db.on('error', function(error) {
-    console.error(
-      chalk.red('Error in MongoDb connection: ' + error)
-    );
-    mongoose.disconnect();
+db.on('error', function (error) {
+	console.error(
+		chalk.red('Error in MongoDb connection: ' + error)
+	);
+	mongoose.disconnect();
 });
 
-db.on('close', function() {
-    console.log(
-      chalk.red('数据库断开，重新连接数据库')
-    );
-    mongoose.connect(config.url, {server:{auto_reconnect:true}});
+db.on('close', function () {
+	console.log(
+		chalk.red('数据库断开，重新连接数据库')
+	);
+	mongoose.connect(config.url, {
+		server: {
+			auto_reconnect: true
+		}
+	});
 });
 
 export default db;
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,17 @@ import metting from './metting'
 export default app => {
 	app.use('/admin', admin);
 	app.use('/metting', metting);
+	// 定制404页面
+	app.use(function(req, res){
+		res.type('text/plain');
+		res.status(404);
+		res.send('404 - Not Found');
+	});
+	//定制500页面
+	app.use(function(err, req, res, next){
+		console.error(err.stack);
+		res.type('text/plain');
+		res.status(500);
+		res.send('500 - Server Error');
+	});
 }
